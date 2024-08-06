02eb6023f42a9ec1a54423e8f62f3051cfa9083f
helpotcreator <helpotcreator@gmail.com>
2024-08-06 12:38:25 +0900

    Feat: update

--- submission/0.8979/kkh-3-data-preprocessing.py
+++
@@ -1,8 +0,0 @@
-# train['target'][train['ID'] == '45f0d2dfc7e47c03.jpg'] = 7
-# train['target'][train['ID'] == 'aec62dced7af97cd.jpg'] = 14
-# train['target'][train['ID'] == '8646f2c3280a4f49.jpg'] = 3
-# train['target'][train['ID'] == '1ec14a14bbe633db.jpg'] = 7
-# train['target'][train['ID'] == '7100c5c67aecadc5.jpg'] = 7
-# train['target'][train['ID'] == 'c5182ab809478f12.jpg'] = 14
-# train['target'][train['ID'] == '38d1796b6ad99ddd.jpg'] = 10
-# train['target'][train['ID'] == '0583254a73b48ece.jpg'] = 10
